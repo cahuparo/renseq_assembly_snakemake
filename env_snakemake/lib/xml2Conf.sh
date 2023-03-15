84518a7f2e856072e012d37869954e255862dcf9
cahuparo <chparada@ncsu.edu>
2023-03-15 10:15:45 -0400

    update git

---
+++ env_snakemake/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/data/run/chparada/renseq_assembly_snakemake/env_snakemake/lib"
+XML2_LIBS="-lxml2 -L/data/run/chparada/renseq_assembly_snakemake/env_snakemake/lib -lz -L/data/run/chparada/renseq_assembly_snakemake/env_snakemake/lib -llzma -lpthread -L/data/run/chparada/renseq_assembly_snakemake/env_snakemake/lib -L/data/run/chparada/renseq_assembly_snakemake/env_snakemake/lib -licui18n -licuuc -licudata -lm "
+XML2_INCLUDEDIR="-I/data/run/chparada/renseq_assembly_snakemake/env_snakemake/include/libxml2 -I/data/run/chparada/renseq_assembly_snakemake/env_snakemake/include"
+MODULE_VERSION="xml2-2.9.14"
+
