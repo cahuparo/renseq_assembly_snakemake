cf7fc3d2f663d522e00edddb4a40bbf1661fa6f4
cahuparo <chparada@ncsu.edu>
2023-03-15 10:54:06 -0400

    update

--- code/install_snakemake.bash
+++ code/install_snakemake.bash
@@ -5,4 +5,3 @@ conda create --prefix /data/run/chparada/renseq_assembly_snakemake/env_snakemake
 conda activate /data/run/chparada/renseq_assembly_snakemake/env_snakemake
 conda install -c bioconda snakemake
 conda deactivate
-somethinh
