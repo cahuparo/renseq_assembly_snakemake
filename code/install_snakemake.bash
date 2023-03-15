d2c17db2d6608e7111c3721b238d2157610b7e18
cahuparo <chparada@ncsu.edu>
2023-03-15 10:50:12 -0400

    update

--- code/install_snakemake.bash
+++ code/install_snakemake.bash
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 
+conda env create --prefix /data/run/chparada/renseq_assembly_snakemake/env_snakemake -f dependencies.yaml
 conda create --prefix /data/run/chparada/renseq_assembly_snakemake/env_snakemake
 conda activate /data/run/chparada/renseq_assembly_snakemake/env_snakemake
 conda install -c bioconda snakemake
